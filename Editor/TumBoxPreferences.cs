a34b4330123556070285a6d051c1741a10d58937
Tomasz Kierpiec <kierpus3@gmail.com>
2023-11-15 11:29:42 +0100

    Remove unnecessery files

--- Editor/TumBoxPreferences.cs
+++
@@ -1,6 +0,0 @@
-﻿
-namespace TumBox.Extensions {
-	public static class TumBoxPreferences {
-		public static string HomeFolder => "Packages/com.atom3y.tumbox/";
-	}
-}
--- Runtime/Extensions/StringExtensions.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace TumBox.Extensions {
-    public static class StringExtensions {        
-        public static string WithColor(this string text, Color color) {
-            return $"<color=#{ColorUtility.ToHtmlStringRGB(color)}>{text}</color>";
-        }
-    }
-}
